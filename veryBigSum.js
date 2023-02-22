7d52728fea8a598f868a9ae55eacc19f9ee94c94
Arun-kumar-19 <118425866+Arun-kumar-19@users.noreply.github.com>
2023-02-23 00:16:05 +0530

    commit

--- veryBigSum.js
+++ veryBigSum.js
@@ -1,1 +1,11 @@
 // didn't understand the question
+let n = 8;
+let str = "";
+for (let i = 1; i <= n; i++) {
+  str += i + " ";
+  let space = "";
+  for (let j = i; j <= n; j++) {
+    space += " ";
+  }
+  console.log(space + str);
+}
